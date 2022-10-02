4c402d47e734f9abefb638d8ce19020f73527c4a
KrilRoman <romazahaev@gmail.com>
2022-10-02 22:07:29 +0300

    Laba 3

---
+++ script.js
@@ -0,0 +1,9 @@
+let i=0;
+let name2='';
+while(i<64){
+    name2=name2+"#_#_#_#_#_\n";
+    i=i+8;
+    name2=name2+"_#_#_#_#_#\n";
+    i=i+8;
+}
+console.log(name2)
---
+++ script2.js
@@ -0,0 +1,9 @@
+let i2=1;
+while (i2<100){
+    console.log(i2);
+    i2=i2+1;
+    if(i2%3==0){
+        console.log('Hello');
+        i2=i2+1;
+    }
+}
