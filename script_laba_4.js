c7a2cda6032cef544a5907f34b846c6c4bd19c1b
KrilRoman <romazahaev@gmail.com>
2022-10-17 14:52:32 +0300

    NEw 4

--- script_laba_4.js
+++ script_laba_4.js
@@ -144,19 +144,150 @@ function func8(){
 }
 
 function func9(){
-    let test=true;
-    console.log('E9');
-    console.log('test is', true);
-    if(test=true){
-        console.log(test);
-    }
-    test = false;
-    if(test!=true){
-        console.log(test)
+
+    console.log('E9')
+
+    console.log('test is TRUE:')
+    let test=true
+    console.log(test)
+    console.log(!test)
+
+    if (test){
+        console.log(test,'1 way')
     }
 
+    console.log('test is FALSE:')
+    test=false
+    console.log(test)
+    console.log(!test)
+
+    if (!test){
+        console.log(test,'2 way')
+    }
+    
 
 }
+
+function func10(){
+
+    console.log('E10')
+
+    test=false
+    console.log('test is FALSE')
+    console.log(!test)
+    console.log(test)
+    if (!test){
+        console.log(test,'1 way')
+    }
+
+    test=true
+    console.log('test is TRUE')
+    console.log(!test)
+    console.log(test)
+    if (test){
+        console.log(test,'2 way')
+    }
+}
+
+function func11(){
+    console.log('E11')
+    let a = [5,0,-3,2]
+
+    for( i in a){
+        if(5>a[i]>0){
+            console.log(true)
+        }
+        else{
+            console.log(false)
+        }
+    }
+}
+
+function func12(){
+    console.log('E12')
+    let a = [5,0,-3,2]
+
+    for( i in a){
+        if(a[i]==0||a[i]==2){
+            console.log(a[i]+7,'TRUE')
+        }
+        else{
+            console.log(a[i]/10,'FALSE')
+        }
+    }
+
+}
+
+function func13(){
+    console.log('E13')
+    let a=[1,0,3]
+    let b=[3,6,5]
+    for(i in a){
+        if(a[i]<=1&&b[i]>=3){
+            console.log(a[i]+b[i],'TRUE')
+        }
+        else{
+            console.log(a[i]-b[i],'FALSE')
+        }
+    }
+}
+
+function func14(){
+    console.log('E14')
+    let a=[1,4,9,13]
+    let b=[3,6,13,16]
+    
+    for(i in a){
+        if(11>a[i]&&a[i]>2){
+            console.log(a[i],"TRUE")
+        }
+
+        else{if(14>b[i]&&b[i]>=6){
+            console.log(b[i],"TRUE")
+        }
+        else{
+            console.log('FALSE')
+        }
+        }
+    }
+}
+
+function func15(){
+    console.log('E15')
+
+    let result=''
+
+    let num=[1,2,3,4]
+    
+    
+    for (j in num){
+        switch(num[j]){
+            case 1:
+                result+='Зима '
+
+                break
+            case 2:
+                result+='Весна '
+
+                break
+            case 3:
+                result+="Літо "
+
+                break
+            case 4:
+                result+="Осінь "
+
+                break
+        }
+    }
+
+console.log(result)
+}
+
+
+
+
+
 
 func1()
 
@@ -175,3 +306,15 @@ func7()
 func8()
 
 func9()
+
+func10()
+
+func11()
+
+func12()
+
+func13()
+
+func14()
+
+func15()
